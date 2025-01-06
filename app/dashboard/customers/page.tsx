96bf5e0279116fd441e0a8221705c723b79546d1
MohamedAboSeada <madoaxel287@gmail.com>
2025-01-06 21:13:25 +0000

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+	return (
+		<h2 className="text-2xl text-blue-700 font-[700] bg-blue-300 py-2 px-5 w-fit mx-auto my-5 rounded-full">
+			Customers Page
+		</h2>
+	);
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+	return (
+		<h2 className="text-2xl text-slate-700 font-[700] bg-slate-300 py-2 px-5 w-fit mx-auto my-5 rounded-full">
+			Dashboard Page
+		</h2>
+	);
+}
